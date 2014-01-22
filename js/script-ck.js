37f837f68c2fb6a91e30637ea35055b0c91b1054
dje174 <jeromepoucet@gmail.com>
2014-01-22 20:43:17 +0100

    Reglage problème dans firefox

--- js/script-ck.js
+++ js/script-ck.js
@@ -4,4 +4,5 @@
  *
  * coded by Jérôme Poucet
  * started at 26.10.2013
- *//* jshint boss: true, curly: true, eqeqeq: true, eqnull: true, immed: true, latedef: true, newcap: true, noarg: true, browser: true, jquery: true, noempty: true, sub: true, undef: true, unused: true, white: false */function save_product(){var e=document.getElementsByClassName("produit");localStorage.setItem("nameProduct",JSON.stringify(figcaption.value));var t=localStorage.getItem("name");console.log("retrieveProduct:",JSON.parse(t))};
+ *//* jshint boss: true, curly: true, eqeqeq: true, eqnull: true, immed: true, latedef: true, newcap: true, noarg: true, browser: true, jquery: true, noempty: true, sub: true, undef: true, unused: true, white: false */// var product = document.getElementById('nameProduct').innerHTML;
+function save_product(){if(!localStorage)return!1;localStorage.setItem("nameProduct",JSON.stringify(product));var e=localStorage.getItem("nameProduct"),t=document.getElementById("nombreArticles");console.log("retrieveProduct:",JSON.parse(e))}var product=document.getElementById("nameProduct").innerHTML,button=document.getElementsByClassName("add");button=window.document.addEventListener("click",save_product,!1);
--- js/script.js
+++ js/script.js
@@ -7,18 +7,27 @@
  */
 
 /* jshint boss: true, curly: true, eqeqeq: true, eqnull: true, immed: true, latedef: true, newcap: true, noarg: true, browser: true, jquery: true, noempty: true, sub: true, undef: true, unused: true, white: false */
+// var product = document.getElementById('nameProduct').innerHTML;
+var product = document.getElementById('nameProduct').innerHTML;
+var button = document.getElementsByClassName('add');
 
 function save_product(){
-	var product = document.getElementsByClassName('produit');
-
+	if(!localStorage){
+		return false;
+	}
 	// On met l'objet dans le storage
-	localStorage.setItem("nameProduct", JSON.stringify(figcaption.value));
+	localStorage.setItem("nameProduct", JSON.stringify(product));
 
 	// On reprend l'objet du storage
-	var retrieveProduct = localStorage.getItem("name");
+	var retrieveProduct = localStorage.getItem("nameProduct");
+
+	var numberArticles= document.getElementById('nombreArticles');
 
 	console.log('retrieveProduct:', JSON.parse(retrieveProduct));
+
 }
+
+button = window.document.addEventListener('click', save_product, false);
 
 
 	// var dataGroup = document.getElementsByClassName('produit')[0];
